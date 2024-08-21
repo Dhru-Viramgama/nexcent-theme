a342ff6491ef0850b5aabb59293ca3c8e61533c6
Dhru Viramgama <dhru@linearloop.io>
2024-08-21 20:43:24 +0530

    remove canvas-confetti effect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,42 +1,42 @@
 import "@/styles/globals.scss";
 import { NextUIProvider } from "@nextui-org/system";
 import type { AppProps } from "next/app";
-import { useEffect } from "react";
-import confetti from 'canvas-confetti';
+// import { useEffect } from "react";
+// import confetti from 'canvas-confetti';
 import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const handleConfetti = () => {
-    var end = Date.now() + (1 * 1000);
+  // const handleConfetti = () => {
+  //   var end = Date.now() + (1 * 1000);
 
-    // go Buckeyes!
-    var colors = ['#bb0000', '#ffffff'];
+  //   // go Buckeyes!
+  //   var colors = ['#bb0000', '#ffffff'];
 
-    (function frame() {
-      confetti({
-        particleCount: 2,
-        angle: 60,
-        spread: 55,
-        origin: { x: 0 },
-        colors: colors
-      });
-      confetti({
-        particleCount: 2,
-        angle: 120,
-        spread: 55,
-        origin: { x: 1 },
-        colors: colors
-      });
+  //   (function frame() {
+  //     confetti({
+  //       particleCount: 2,
+  //       angle: 60,
+  //       spread: 55,
+  //       origin: { x: 0 },
+  //       colors: colors
+  //     });
+  //     confetti({
+  //       particleCount: 2,
+  //       angle: 120,
+  //       spread: 55,
+  //       origin: { x: 1 },
+  //       colors: colors
+  //     });
 
-      if (Date.now() < end) {
-        requestAnimationFrame(frame);
-      }
-    }());
-  };
+  //     if (Date.now() < end) {
+  //       requestAnimationFrame(frame);
+  //     }
+  //   }());
+  // };
 
-  useEffect(() => {
-    handleConfetti();
-  })
+  // useEffect(() => {
+  //   handleConfetti();
+  // })
 
   return (
     <>
